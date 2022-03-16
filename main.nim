008c3fa25412a527d4f3f1e1063ce4e55fea7495
Derek Morey <derek.o.morey@gmail.com>
2022-03-15 22:12:14 -0400

    Got a rectangle bouncing left and right

--- main.nim
+++ main.nim
@@ -3,6 +3,22 @@ import sdl2
 const WINDOW_WIDTH = 800
 const WINDOW_HEIGHT = 600
 
+type
+  GameScene = ref object of RootObj
+    next: GameScene
+
+method init(this: GameScene): void {.base.} =
+  quit "Override plz"
+
+method events(this: GameScene): void {.base.} =
+  quit "Override plz"
+
+method update(this: GameScene): void {.base.} =
+  quit "Override plz"
+
+method render(this: GameScene, render: RendererPtr): void {.base.} =
+  quit "Override plz"
+
 proc main =
   discard sdl2.init(INIT_EVERYTHING)
 
@@ -18,20 +34,48 @@ proc main =
     h = WINDOW_HEIGHT,
     flags = SDL_WINDOW_SHOWN
     )
-  render = createRenderer(window, -1, Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
+  render = createRenderer(
+    window = window, 
+    index = -1,
+    flags = Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture
+    )
 
   var
     evt = sdl2.defaultEvent
     runGame = true
+    x_incr = 5
+    rect_x = 100
 
   while runGame:
     while pollEvent(evt):
-      if evt.kind == QuitEvent:
+      case evt.kind
+      of QuitEvent:
         runGame = false
         break
+      else:
+        discard
 
+    rect_x = rect_x + x_incr
+
+    if rect_x < 0:
+      rect_x = 0
+      x_incr *= -1
+    elif rect_x + 100 > WINDOW_WIDTH:
+      rect_x = WINDOW_WIDTH - 100
+      x_incr *= -1
     render.setDrawColor(0, 0, 0, 255)
     render.clear
+
+    var
+      my_rect = sdl2.rect(
+        x = cint(rect_x),
+        y = cint(100),
+        w = cint(100),
+        h = cint(100),
+      )
+    render.setDrawColor(255, 0, 0, 255)
+    render.fillRect(my_rect)
+
     render.present
 
   destroy render

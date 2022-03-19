ab476dd5a6c5234002101698e881167170b285c0
Derek Morey <derek.o.morey@gmail.com>
2022-03-19 11:47:34 -0400

    Basic ECS architecture is there kinda?
    nothing is being drawn to screen tho

--- main.nim
+++ main.nim
@@ -1,29 +1,34 @@
+import std/tables
+import system/iterators
+
 import sdl2
 
 const WINDOW_WIDTH = 800
 const WINDOW_HEIGHT = 600
 
 type
-  GameScene = ref object of RootObj
-    next: GameScene
-
-method init(this: GameScene): void {.base.} =
-  quit "Override plz"
-
-method events(this: GameScene): void {.base.} =
-  quit "Override plz"
-
-method update(this: GameScene): void {.base.} =
-  quit "Override plz"
-
-method render(this: GameScene, render: RendererPtr): void {.base.} =
-  quit "Override plz"
-
-type
-  GameRect = object
+  ComponentTypes = enum
+    CTPosition, CTDraw
+  ComponentContext = object
+    render: RendererPtr
+  Component = ref object of RootObj
+    entity: Entity
+  Position = ref object of Component
     x, y, w, h, vx, vy: int
+  Draw = ref object of Component
+    r, g, b, a: uint8
 
-proc update(r: var GameRect) =
+  Entity = object
+    id: int
+    components: Table[ComponentTypes, Component]
+
+  GameState = object
+    entities: seq[Entity]
+
+method process(this: var Component, ctx: ComponentContext): void  {.base.} =
+  quit "Override me plz!"
+
+method process(r: var Position, ctx: ComponentContext): void =
   r.x += r.vx
   if r.x < 0:
     r.x = 0
@@ -40,13 +45,33 @@ proc update(r: var GameRect) =
     r.y = WINDOW_HEIGHT - 100
     r.vy *= -1
 
-proc get_rect(r: GameRect): sdl2.Rect =
+method get_rect(this: Position): sdl2.Rect =
   result = sdl2.rect(
-    x = cint(r.x),
-    y = cint(r.y),
-    w = cint(r.w),
-    h = cint(r.h)
+    x = cint(this.x),
+    y = cint(this.y),
+    w = cint(this.w),
+    h = cint(this.h)
   )
+
+method process(this: var Draw, ctx: ComponentContext): void =
+  if not this.entity.components.hasKey(ComponentTypes.CTPosition):
+    echo "No Position"
+    return
+
+  var
+    component = this.entity.components[ComponentTypes.CTPosition]
+    pos = Position(component)
+    rect = pos.get_rect
+
+  ctx.render.setDrawColor(0, 0, 0, 255)
+  ctx.render.clear
+  ctx.render.setDrawColor(this.r, this.g, this.b, this.a)
+  ctx.render.fill_rect(rect)
+  ctx.render.present
+
+proc initEntity(id: int): Entity =
+  var components = initTable[ComponentTypes, Component](64)
+  result = Entity(id: id, components: components)
 
 proc main =
   discard sdl2.init(INIT_EVERYTHING)
@@ -54,14 +79,6 @@ proc main =
   var
     window: WindowPtr
     render: RendererPtr
-    my_rect = GameRect(
-      x: 100,
-      y: 100,
-      w: 100,
-      h: 100,
-      vx: 5,
-      vy: 5,
-    )
 
   window = createWindow(
     title = "SDL Skeleton",
@@ -80,6 +97,15 @@ proc main =
   var
     evt = sdl2.defaultEvent
     runGame = true
+    state = GameState(entities: @[])
+    ctx = ComponentContext(render: render)
+    my_entity = initEntity(0)
+    pos_comp = Position(x: 100, y: 100, w: 100, h: 100, vx: 5, vy: 5)
+    draw_comp = Draw(r: 255, g: 0, b: 0, a: 0)
+
+  my_entity.components[ComponentTypes.CTPosition] = pos_comp
+  my_entity.components[ComponentTypes.CTDraw] = draw_comp
+  state.entities.add(my_entity)
 
   while runGame:
     while pollEvent(evt):
@@ -90,18 +116,12 @@ proc main =
       else:
         discard
 
-    my_rect.update()
-
-    render.setDrawColor(0, 0, 0, 255)
-    render.clear
-
-    var
-      sdl_rect = my_rect.get_rect()
-
-    render.setDrawColor(255, 0, 0, 255)
-    render.fillRect(sdl_rect)
-
-    render.present
+      for component_type in iterators.items(ComponentTypes):
+        for entity in state.entities:
+          var component: Component
+          if component_type in entity.components:
+            component = entity.components[component_type]
+            component.process(ctx)
 
   destroy render
   destroy window
